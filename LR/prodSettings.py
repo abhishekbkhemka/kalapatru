e516623c66b51a5c69317c3134b19532f7cbc95a
Abhishek Khemka <abhishekbkhemka@gmail.com>
2015-11-30 17:51:42 +0530

    new file

---
+++ LR/prodSettings.py
@@ -0,0 +1,12 @@
+
+DATABASES = {
+	'default':{
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME' : 'kalapatru',             # Or path to database file if using sqlite3.
+		'USER' : 'kalapatru',             # Not used with sqlite3.
+		'PASSWORD' : 'Khemka55',         # Not used with sqlite3.
+		'HOST' : 'kalapatru.cdhn6y14k2en.ap-southeast-1.rds.amazonaws.com',             # Set to empty string for localhost. Not used with sqlite3.
+		'PORT' : '' ,            # Set to empty string for default. Not used with sqlite3.
+
+	}
+}
