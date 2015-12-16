c3b9408eb4bff559d67fac4618ae0b21b4335f30
Abhishek Khemka <abhishekbkhemka@gmail.com>
2015-12-16 23:27:42 +0530

    wip

--- LR/models.py
+++ LR/models.py
@@ -153,7 +153,7 @@ class Dispatch(models.Model):
     vanNo = models.CharField(max_length=250,blank=True,null=True)
     name = models.CharField(max_length=250,blank=True,null=True)
     remarks = models.CharField(max_length=250,blank=True,null=True)
-    isLocked = models.BooleanField(default=False)
+    isLocked = models.BooleanField(default=True)
 
     def __str__(self):
         return "%s is Disparch on %s"%(self.name,self.date)
