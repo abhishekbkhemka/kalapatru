ce29b7760a10f0615ee1cdb29ea116fb78710afd
Abhishek Khemka <abhishekbkhemka@gmail.com>
2015-12-25 23:20:30 +0530

    ll

--- LR/TransporterCtl.py
+++ LR/TransporterCtl.py
@@ -3,5 +3,5 @@ from LR.models import Transporter
 class TransporterController():
 
     def getTransporters(self,requestUser):
-        transporters = Transporter.objects.filter().distinct('name').order_by('name')
+        transporters = Transporter.objects.filter().order_by('name')
         return transporters
